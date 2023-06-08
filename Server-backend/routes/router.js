88ef141c7d7a7f3fcacbcbd582be642f6f79949a
Sourav01112 <chaudharysourav997@gmail.com>
2023-06-08 22:23:52 +0530

    implemented Routing

--- Server-backend/routes/router.js
+++ Server-backend/routes/router.js
@@ -4,5 +4,7 @@ const route = express.Router();
 const services = require("../services/render");
 
 route.get("/", services.homeRoutes);
+route.get("/video-chat", services.videoChatRoutes);
+route.get("/text-chat", services.textChatRoutes);
 
 module.exports = route;
--- Server-backend/services/render.js
+++ Server-backend/services/render.js
@@ -3,3 +3,9 @@ const axios = require("axios");
 exports.homeRoutes = (req, res) => {
   res.render("index");
 };
+exports.videoChatRoutes = (req, res) => {
+  res.render("video-chat");
+};
+exports.textChatRoutes = (req, res) => {
+  res.render("text-chat");
+};
